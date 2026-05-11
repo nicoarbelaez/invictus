b3430915473c8ab334334b4f31ab207ba7f551ee
Nicolas Arbelaez <arbelaeznicolas636@gmail.com>
2026-05-11 01:31:04 -0500

    chore(config): actualizar configuración de VSCode y ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -37,6 +37,7 @@ export default [
       'no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
       'unused-imports/no-unused-imports': 'error',
+      'no-undef': 'off',
       'unused-imports/no-unused-vars': [
         'warn',
         { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
