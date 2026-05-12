5beb06c727631ba87397a9bc7a06f4f2ae961c1f
Nicolas Arbelaez <arbelaeznicolas636@gmail.com>
2026-05-11 23:48:37 -0500

    chore(ui): aplicar formato de linter en index, CountBadge y Sheet

--- src/components/global/nav/index.ts
+++ src/components/global/nav/index.ts
@@ -1,9 +1,9 @@
-export { MobileMenuDrawer } from './MobileMenuDrawer';
-export { NavActions } from './NavActions';
-export { NavbarMotion } from './NavbarMotion';
-export { NavBrand } from './NavBrand';
-export { type NavCategory, NavCategoriesBar } from './NavCategoriesBar';
-export { NavDesktop } from './NavDesktop';
-export { NavMobile } from './NavMobile';
-export { NavSearch } from './NavSearch';
-export { useNavScroll } from './useNavScroll';
+export { MobileMenuDrawer } from './MobileMenuDrawer'
+export { NavActions } from './NavActions'
+export { NavbarMotion } from './NavbarMotion'
+export { NavBrand } from './NavBrand'
+export { type NavCategory, NavCategoriesBar } from './NavCategoriesBar'
+export { NavDesktop } from './NavDesktop'
+export { NavMobile } from './NavMobile'
+export { NavSearch } from './NavSearch'
+export { useNavScroll } from './useNavScroll'
